1eb859e5c01c9b9d0bbff3895b4ca100bf6f17c5
JUNSU\dlwns <dlwnstn0315@gmail.com>
2026-02-27 16:21:51 +0900

    update

--- src/test/java/com/app/trycatch/TrycatchApplicationTests.java
+++ src/test/java/com/app/trycatch/TrycatchApplicationTests.java
@@ -3,7 +3,7 @@ package com.app.trycatch;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class TrycatchApplicationTests {
 
     @Test
